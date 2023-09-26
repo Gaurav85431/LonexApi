5a259ab7f3866ad99712325292e1e3948336401c
Gaurav <pushpamgaurav@gmail.com>
2023-09-26 14:24:40 +0530

    my api add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/mySaloon');
+mongoose.connect('mongodb://localhost:27017/mySaloon'); //127.0.0.1
 
 //user routes 
 
